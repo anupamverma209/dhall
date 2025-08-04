982239238a01b846d9ae1d5913ad0759bcb9a33e
Anupam Verma <158498123+anupamverma209@users.noreply.github.com>
2025-08-04 22:10:06 +0530

    chnage in index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const fileRoutes = require("./routes/file");
 const connectDB = require("./config/database");
 const cloudinaryStorage = require("./config/cloudinary");
 const fileupload = require("express-fileupload");
+const fetch = require("node-fetch");
 app.use(cors());
 app.use(express.json());
 
@@ -18,4 +19,28 @@ app.use("/api/v1", fileRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/test-rd", async (req, res) => {
+  try {
+    const response = await fetch(
+      "https://api.realitydefender.com/v1/image/check",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.REALITY_DEFENDER_API_KEY}`,
+        },
+        body: JSON.stringify({
+          image_url:
+            "https://res.cloudinary.com/dnykejpyx/image/upload/v1754323007/deepfake/detection/ompk56i0llq9fdmszwai.jpg",
+        }),
+      }
+    );
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
